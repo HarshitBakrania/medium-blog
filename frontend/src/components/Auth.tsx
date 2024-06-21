681445399d593ea616974cd20b66e6352def7ac3
Harshit Bakrania <harshit.bakrania@gmail.com>
2024-06-22 02:15:42 +0530

    added signup and signin routes

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,60 @@
+import { SignupSchema } from "@h_bakrania/medium-common";
+import { ChangeEvent, useState } from "react";
+import { Link } from "react-router-dom"
+
+export const Auth = ({ type }: {type: "signup" | "signin"}) =>{
+    const [inputs, setInputs] = useState<SignupSchema>({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    return <div className="h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div>
+                <div className="font-bold text-5xl">
+                    Create an account 
+                </div>
+                <div className="flex justify-center text-xl text-slate-600 pt-2">
+                    {type === "signup"? "Already have an account?" : "Dont have an account?"}
+                    <Link className="pl-2 underline" to = {type === "signup" ? "/signin" : "/signup"}>
+                        {type === "signup" ? "Login" : "Sign Up"}
+                    </Link>
+                </div>
+                {type === "signup" ? <LabelledInputBox label="Name" placeholder="Enter your name" onChange={(e) =>{
+                    setInputs({
+                        ...inputs,
+                        name: e.target.value
+                    })
+                }} /> : null}
+                <LabelledInputBox label="Email" placeholder="Enter your email address" onChange={(e) =>{
+                    setInputs({
+                        ...inputs,
+                        email: e.target.value
+                    })
+                }} />
+                <LabelledInputBox label="Password" type={"password"} placeholder="Enter your password" onChange={(e) =>{
+                    setInputs({
+                        ...inputs,
+                        password: e.target.value
+                    })
+                }} />
+                <button type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign Up" : "Sign in"}</button>
+            </div>
+        </div>
+    </div>
+}
+
+interface LabelledInputType {
+    label: string,
+    placeholder: string,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    type?: string
+}
+
+function LabelledInputBox({ label, placeholder, onChange, type}: LabelledInputType){
+    return <div>
+        <label className="block mb-2 text-base font-semibold text-black pt-5">{label}</label>
+        <input onChange={onChange} type={ type || "text"} id="first_name" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+    </div>
+}
---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,17 @@
+export const Quote = () =>{
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-4xl font-bold">
+                    "The customer service i received was exceptional. The support team went above and beyond to address my concerns."
+                </div>
+                <div className="max-w-md text-2xl font-semibold mt-4">
+                    Jules Winnfield
+                </div>
+                <div className="max-w-md text-base font-semibol">
+                    CEO | Acme corp
+                </div>  
+            </div>
+        </div>  
+    </div>
+}
